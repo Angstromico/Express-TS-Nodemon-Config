a479c24424e47cd69dc91d69f0e495a4c4842ff0
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2024-11-01 12:11:57 -0400

    Add .env

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,11 @@ import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
 import compression from 'compression'
 import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config() // This will load the .env file into process.env
+
+const PORT = process.env.PORT
 
 const app = express()
 
@@ -18,3 +23,7 @@ app.use(cookieParser())
 app.use(bodyParser.json())
 
 const server = http.createServer(app)
+
+server.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}/`)
+})
