302acbe84d728c197f1a004fcc8eb24ae86333a1
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2024-11-01 13:18:43 -0400

    Add Mongoo

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser'
 import compression from 'compression'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import mongoose from 'mongoose'
 
 dotenv.config() // This will load the .env file into process.env
 
@@ -27,3 +28,9 @@ const server = http.createServer(app)
 server.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}/`)
 })
+
+const MONGO_URL = process.env.MONGO_URL
+
+mongoose.Promise = Promise
+mongoose.connect(MONGO_URL)
+mongoose.connection.on('error', (error: Error) => console.log(error))
